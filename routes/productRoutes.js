2a68e48279852eb03090d22abbe4b58e8417034b
Diego Perales <diegoperales878@gmail.com>
2022-09-11 20:46:02 +0200

    Añadiendo las funciones mongoose

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,9 +3,14 @@ const productController=require("../controllers/productController");
 const productRouter=express.Router();
 
 //Routes
-productRouter.route("/").get(productController.getAllProducts).post(productController.addProduct);
+productRouter.route("/")
+    .get(productController.getAllProducts)
+    .post(productController.addProduct);
 //productRouter.post("/",addProduct);
-productRouter.route("/:id").get(productController.getProductById).delete(productController.deleteProductById).put(productController.updateProductById);
+productRouter.route("/:id")
+    .get(productController.getProductById)
+    .delete(productController.deleteProductById)
+    .put(productController.updateProductById);
 //
 
 
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const mongoose=require("mongoose");
 const app  =require("./app");
+const Product =require("./models/Product");
 const port = process.env.PORT;
-
+mongoose.connect(process.env.DATABASE,{}).then((con)=>{
+  console.log("Conected to mongo");
+  // const p=new Product({productName:"product 3",price:20});
+  // p.save().then(()=>{
+  //   console.log("saved");
+  // })
+  
+}) 
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
