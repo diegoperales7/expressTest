aa6eaf3f4d681a0f9919726367bc4e1f7f08c373
Diego Perales <diegoperales878@gmail.com>
2022-09-19 15:55:07 +0200

    error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const morgan=require("morgan");
 const productRouter = require("./routes/productRoutes");
+const MyError = require("./utils/MyError");
 const app = express();
 //const module=require("module");
 
@@ -9,13 +10,30 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use((req,res,next)=>{
-  req.requestTime=new Date ().toISOString();
+  req.requestTime=new Date().toISOString();
   next();
 });
 
 //routes
 app.use("/api/v1/products/",productRouter);
 
+app.all("*",(req,res,next)=>{
+  next(new MyError("route not found",404)); 
+});
+
+app.use((err,req,res,next)=>{
+  if(process.env.NODE_ENV=="development"){
+    res.status(err.statusCode).json({
+      status: err.status,
+      message:err.message,
+    });
+  }else{
+    res.status(err.statusCode).json({
+      status: err.status,
+      message:"Ocurrio un error",
+    });
+  }
+});
 //console.log(module);
 
 module.exports=app;
