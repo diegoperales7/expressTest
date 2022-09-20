0bd14e9898870cf8f44a5f0429f41ffb91e37099
Diego Perales <diegoperales878@gmail.com>
2022-09-20 20:26:18 +0200

    server error management

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const mongoose=require("mongoose");
-const app  =require("./app");
+
+process.on("uncaughtException",(err)=>{
+  console.log("uncaughtException",err);
+  console.log("Shutting down");
+  process.exit(1);
+});
+
 const Product =require("./models/Product");
+const app  =require("./app");
 const port = process.env.PORT;
+
+
 mongoose.connect(process.env.DATABASE,{}).then((con)=>{
   console.log("Conected to mongo");
-  // const p=new Product({productName:"product 3",price:20});
-  // p.save().then(()=>{
-  //   console.log("saved");
-  // })
-  
-}) 
+});
 
-app.listen(port, () => {
+const server=app.listen(port, () => {
     console.log(`App running on port ${port}`);
+});
+
+process.on("unhandledRejection",(err)=>{
+  console.log("unhandledRejection",err);
+  console.log("Shutting down");
+  server.close(()=>{
+    process.exit(1);
   });
+});
